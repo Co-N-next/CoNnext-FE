d8137e64bcf91039ff78c431db2dff9c4a718908
unknown <juujuuprin0818@gmail.com>
2026-02-10 02:04:43 +0900

    [#32] style : footer 아이콘 및 layout 컴포넌트 정리

--- coNnext/src/components/layout/Header.tsx
+++ coNnext/src/components/layout/Header.tsx
@@ -4,16 +4,12 @@ import { useNavigate } from 'react-router-dom';
 import Connext from '../../assets/logo/Con-next.svg';
 import Ticket from '../../assets/logo/TicketLogo.svg';
 import Bell from '../../assets/logo/BellLogo.svg';
-<<<<<<< Updated upstream
-import Menu from '../../assets/logo/MenuLogo.svg';
-=======
->>>>>>> Stashed changes
 
 const Header: React.FC = () => {
   const navigate = useNavigate(); 
 
   return (
-    <header className="fixed top-0 left-1/2 -translate-x-1/2 z-50 bg-[#0E172A] w-full max-w-[450px] px-6 py-4">
+    <header className="fixed top-0 left-1/2 -translate-x-1/2 z-50 bg-[#0E172A] w-full max-w-112.5 px-6 py-4">
       <div className="flex items-center justify-between w-full">
         {/* 로고 */}
         <div className="flex items-center">
@@ -27,13 +23,13 @@ const Header: React.FC = () => {
         </div>
 
         {/* 아이콘 */}
-        <div className="flex items-center gap-[10px] w-[92px] h-[24px] justify-end">
+        <div className="flex items-center gap-2.5 w-23 h-6 justify-end">
         
-          <button className="w-6 h-6 p-0 hover:opacity-80 transition-opacity flex-shrink-0">
+          <button className="w-6 h-6 p-0 hover:opacity-80 transition-opacity shrink-0">
             <img src={Ticket} alt="Ticket" className="w-6 h-6 block" />
           </button>
           
-          <button className="w-6 h-6 p-0 hover:opacity-80 transition-opacity flex-shrink-0">
+          <button className="w-6 h-6 p-0 hover:opacity-80 transition-opacity shrink-0">
             <img src={Bell} alt="Bell" className="w-6 h-6 block" />
           </button>
         
